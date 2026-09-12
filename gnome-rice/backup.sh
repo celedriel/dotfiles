6b55a98bc2badf64b54cce51e831c317d840b515
celedriel <vanessa.miranda5690@gmail.com>
2026-09-12 12:37:24 -0300

    refactor: centraliza arquivos ini no gnome-rice e automatiza scripts de dconf

--- gnome-rice/backup.sh
+++ gnome-rice/backup.sh
@@ -1,11 +1,21 @@
 #!/bin/zsh
 
 echo "Fazendo backup das configurações do GNOME..."
-
 cd ~/dotfiles/gnome-rice/
 
-dconf dump /org/gnome/shell/extensions/ > extensions.ini
+# Interface e Janelas
 dconf dump /org/gnome/desktop/interface/ > interface.ini
 dconf dump /org/gnome/desktop/wm/preferences/ > wm-preferences.ini
+
+# Extensões (Isso já engloba Pop Shell, Tilingshell e outras)
+dconf dump /org/gnome/shell/extensions/ > all-extensions-settings.ini
+gnome-extensions list --enabled > enabled-extensions-list.txt
+
+# Atalhos e Teclado
+dconf dump /org/gnome/desktop/wm/keybindings/ > gnome-keybindings.ini
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > custom-media-keys.ini
+
+# Nautilus
+dconf dump /org/gnome/nautilus/ > nautilus-preferences.ini
 
 echo "Backup concluído com sucesso!"
--- gnome-rice/restore.sh
+++ gnome-rice/restore.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
 echo "Restaurando configurações do GNOME..."
-
 cd ~/dotfiles/gnome-rice/
 
-dconf load /org/gnome/shell/extensions/ < extensions.ini
 dconf load /org/gnome/desktop/interface/ < interface.ini
 dconf load /org/gnome/desktop/wm/preferences/ < wm-preferences.ini
+dconf load /org/gnome/shell/extensions/ < all-extensions-settings.ini
+dconf load /org/gnome/desktop/wm/keybindings/ < gnome-keybindings.ini
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < custom-media-keys.ini
+dconf load /org/gnome/nautilus/ < nautilus-preferences.ini
 
 echo "GNOME restaurado com sucesso! (Talvez seja necessário deslogar para aplicar tudo)"
